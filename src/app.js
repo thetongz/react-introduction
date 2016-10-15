57f94ee26d5279c476cc64c3639e36bb9f01ec8a
thetongz <miiz_tongz@live.com>
2016-10-15 12:04:19 +0700

    update

--- src/app.js
+++ src/app.js
@@ -2,11 +2,66 @@ const name = "TheTonG-React"
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Test from './test'
 
-const TheTong = () => {
-    return <div> <h1>My react app</h1> </div>
+const Searchform = () => {
+    return (
+        <form>
+            <input type="text" />
+            <button type="submit">Search</button>
+        </form>
+    )
 }
 
-ReactDOM.render(<TheTong /> , document.getElementById('app'))
-ReactDOM.render(<Test /> , document.getElementById('apps'))
+const Header = (props) => (
+    <header>
+        <h1>{props.name}</h1>
+        <Searchform />
+    </header>
+)
+
+const Items = (props) => (
+    <ul>
+        {
+            props.items.map(items => (
+                    <li>{ items }</li>
+                )
+            )
+        }
+    </ul>
+)
+
+const Content = (props) => (
+    <section>
+        <p>{props.content.toTimeString()}</p>
+        <Items items = {props.items}/>
+    </section>
+)
+
+const App = () => {
+    const name = "frontech"
+    const content = new Date()
+    const items = [
+        "Kahn",
+        "John",
+        "Leica",
+        "Mercy"
+    ]
+
+    return(
+        <section>
+            <Header name = {name}/>
+            <Content 
+                content = {content}
+                items = {items}
+            />
+        </section>
+    )
+}
+
+
+
+function run () {
+    ReactDOM.render(<App/> , document.getElementById('app'))
+}
+
+setInterval(run , 500)
