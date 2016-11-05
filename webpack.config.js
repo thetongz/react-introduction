774c9ebb9cdd030443dce53b54ea09183dec2ca5
thetongz <miiz_tongz@live.com>
2016-11-05 10:24:05 +0700

    extrack css from JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var config = {
     entry: {
-        app: path.join(__dirname, 'src', 'app.js'),
+        hello: path.join(__dirname, 'src', 'app.js'),
     },
     module: {
         loaders: [
@@ -12,21 +13,22 @@ var config = {
                 include: path.join(__dirname, 'src'),
                 loader: 'babel-loader',
                 query: {
-                    presets: ['es2015','react']
+                    presets: ['es2015', 'react']
                 }
             },
             {
                 test: /\.css$/,
                 include: path.join(__dirname, 'src'),
-                loaders: [
-                    'style','css'
-                ]
+                loader: ExtractTextPlugin.extract('style', 'css')
             }
         ]
     },
+    plugins: [
+        new ExtractTextPlugin('app.css')
+    ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: "app.js"
+        filename: 'app.js'
     },
     devServer: {
         historyApiFallback: true,
